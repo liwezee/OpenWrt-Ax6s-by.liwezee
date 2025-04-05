bf3e7373008200637cd530f8cc1bd0618e0197a8
liwezee <63093684+liwezee@users.noreply.github.com>
2025-04-05 22:37:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,14 +4,14 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
- Copyright (c) 2019-2025 Liwezee <https://liwezee.cn>
+# Copyright (c) 2019-2025 Liwezee <https://liwezee.cn>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
 
 # Modify default IP
- sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
