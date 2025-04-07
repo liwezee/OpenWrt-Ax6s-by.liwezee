26328eba0a1c5b7261c1ad7b47e05c14bb274869
liwezee <63093684+liwezee@users.noreply.github.com>
2025-04-07 23:41:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,6 @@
 
 # Uncomment a feed source
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
- rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
